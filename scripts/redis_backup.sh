d8774d9d4d19b6525ca193bc9f875b05a520d4fd
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2022-09-10 10:03:08 +0530

    Changed slave names

--- scripts/redis_backup.sh
+++ scripts/redis_backup.sh
@@ -10,7 +10,7 @@ port=$2
 echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[master] Being slave state..." >> $LOGFILE 2>&1
 echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[master] wait 10 sec for data sync from old master" >> $LOGFILE 2>&1
 sleep 10
-echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[master] data rsync from old mater ok..." >> $LOGFILE 2>&1
-echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[slaver] Run 'SLAVEOF $host $port'" >> $LOGFILE 2>&1
+echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[master] data rsync from old master ok..." >> $LOGFILE 2>&1
+echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[slave] Run 'SLAVEOF $host $port'" >> $LOGFILE 2>&1
 $REDISCLI SLAVEOF $host $port >> $LOGFILE  2>&1
-echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[slaver] slave connect to $host ok..." >> $LOGFILE 2>&1
+echo "`date +'%Y-%m-%d %H:%M:%S'`|$pid|state:[slave] slave connect to $host ok..." >> $LOGFILE 2>&1
